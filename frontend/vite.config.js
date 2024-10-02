56177889e65aebd07b0ec78909a5b0a4549e8864
Zahidul <zahidulxpro@gmail.com>
2024-10-02 14:56:12 +0600

    Initial commit from zip file

---
+++ frontend/vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+
+export default defineConfig({
+  plugins: [vue()],
+  server: {
+    proxy: {
+      '/api': 'http://localhost:5000',
+    },
+  },
+  optimizeDeps: {
+    include: ['vue', 'vue-router', 'axios'],
+  },
+});
